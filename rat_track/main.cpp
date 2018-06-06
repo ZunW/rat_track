6b603dea3dde6bd5dc19fde1e510f88819bff40e
Zun Wang <1183387976@qq.com>
2018-06-06 10:20:33 -0400

    Initial Commit

---
+++ rat_track/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rat_track
+//
+//  Created by Zun Wang on 2018-06-06.
+//  Copyright © 2018 Zun Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
